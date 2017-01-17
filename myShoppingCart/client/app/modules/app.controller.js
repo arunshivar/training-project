5092cab6b4c61f677899520180cbd15be2a2cb86
arunshivar <arun.rapelli@semanticbits.com>
2017-01-17 13:52:18 +0530

    Trainng project

---
+++ myShoppingCart/client/app/modules/app.controller.js
@@ -0,0 +1,17 @@
+/**
+ * Created by sb-c2-02 on 17/1/17.
+ */
+
+(function ()
+{
+    angular
+        .module('myshop')
+        .controller('appController',appController);
+
+    function appController()
+    {
+        console.log("App Controller");
+    }
+
+}());
+
