235fabf0f9f7eaf43a6933b679c0325e8c41c44d
arunshivar <arun.rapelli@semanticbits.com>
2017-01-19 13:04:57 +0530

    Trainng project Jan 18

--- myShoppingCart/client/app/modules/home/home.controller.js
+++ myShoppingCart/client/app/modules/home/home.controller.js
@@ -5,17 +5,33 @@
 (function()
 {
     "use strict";
-   angular
-       .module('homeModule')
-       .controller('HomeController',HomeController);
+    angular
+        .module('homeModule')
+        .controller('HomeController',HomeController);
 
 
-    HomeController.$inject = ['homeFactory'];
-    function HomeController(homeFactory)
+    HomeController.$inject = ['homeFactory','$rootScope'];
+    function HomeController(homeFactory,$rootScope)
     {
         var vm = this;
         console.log("In home controller");
-        homeFactory.getData();
+        /* homeFactory.getData();*/
+        vm.getAlbums = function() {
+            homeFactory.getData()
+                .then(function(data)
+                    {
+                        $rootScope.products = {};
+                        $rootScope.products = data;
+                        console.log('albums returned to controller.'+data);
+
+                    },
+                    function(data) {
+                        console.log('albums retrieval failed.')
+                    });
+        };
+
+        vm.getAlbums();
+
 
     }
 }());
